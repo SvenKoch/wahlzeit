8bf663395be96d69168f4280ceac1d07a7e0a2ee
McMorgenmantel <svenkoch1995@gmail.com>
2016-10-29 22:59:20 +0200

    added distance algorithm and first tests

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -40,8 +40,20 @@ public class Coordinate {
 	 */
 	public double getDistance(Coordinate other){
 		double deltaLongitude = Math.abs(this.longitude-other.getLongitude());
-		double centralAngle = Math.arccos(Math.sin(this.latitude)*Math.sin(other.latitude)+Math.cos(this.longitude)*Math.cos(other.longitude)*Math.cos(deltaLongitude));
-		return centralAngle * EARTHRADIUS;
-		//TODO: precise algorithm
+		double phi1 = this.latitude;
+		double phi2 = other.getLatitude();
+		
+		// simple algorithm
+		// double centralAngle = Math.acos(Math.sin(this.latitude)*Math.sin(other.latitude)+Math.cos(this.longitude)*Math.cos(other.longitude)*Math.cos(deltaLongitude));
+		
+		// precise algorithm
+		double temp1 = Math.cos(phi2)*Math.sin(deltaLongitude);
+		temp1 = temp1*temp1;
+		double temp2 = Math.cos(phi1)*Math.sin(phi2) - Math.sin(phi1)*Math.cos(phi2)*Math.cos(deltaLongitude);
+		temp2 = temp2*temp2;
+		
+		double centralAngle = Math.atan(Math.sqrt(temp1 + temp2) / (Math.sin(phi1) * Math.sin(phi2)+Math.cos(phi1)*Math.cos(phi2)*Math.cos(deltaLongitude)));
+		
+		return Math.abs(centralAngle * EARTHRADIUS);
 	}
 }
--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -11,12 +11,23 @@ import static org.junit.Assert.assertEquals;
  * Test cases for the Coordinate class.
  */
 public class CoordinateTest {
+	
+	private final int EARTHRADIUS = 6371000;
 
 	/**
 	 *
 	 */
 	@Test
-	public void testGetDistance {
+	public void testGetDistance() {
 		//TODO: tests
+		Coordinate zerozero = new Coordinate(0,0);
+		assertEquals(0.0, zerozero.getDistance(zerozero), 0.0);
+		
+		Coordinate first = new Coordinate(0, 90);
+		assertEquals(0.0, first.getDistance(first), 0.0);
+		
+		assertEquals(2*EARTHRADIUS*Math.PI / 4.0, first.getDistance(zerozero), 0.0);
+		
+		System.out.println(first.getDistance(zerozero));
 	}
 }
