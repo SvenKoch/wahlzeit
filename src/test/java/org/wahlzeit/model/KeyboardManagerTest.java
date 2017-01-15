d8d21ccfba79cb90434e8262de0b5b65c193137d
McMorgenmantel <svenkoch1995@gmail.com>
2017-01-15 15:48:02 +0100

    added keyboard filtering test

--- src/test/java/org/wahlzeit/model/KeyboardManagerTest.java
+++ src/test/java/org/wahlzeit/model/KeyboardManagerTest.java
@@ -2,6 +2,8 @@ package org.wahlzeit.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
 import org.junit.Test;
 
 public class KeyboardManagerTest {
@@ -42,6 +44,27 @@ public class KeyboardManagerTest {
 		assertTrue(k==k2);
 		assertTrue(manager.hasKeyboard(k));
 		assertTrue(manager.hasKeyboard(k2));
-
 	}
+	
+	@Test 
+	public void testFiltering(){
+		KeyboardManager manager = KeyboardManager.getInstance();
+		String manufacturer = "Roccat";
+		String model = "Ryos MK Pro";
+		int numberOfKeys = 133;
+		boolean hasNumPad= true;
+		String serialNumber = "1234567890";
+		String serialNumber2 = "0987654321";
+		String owner = "John Doe";
+		KeyboardType kt = manager.getKeyboardType(manufacturer, model, numberOfKeys, hasNumPad);
+		Keyboard k = manager.getKeyboard(kt, serialNumber, owner);
+		Keyboard k2 = manager.getKeyboard(kt, serialNumber2, owner);
+		
+		Set<Keyboard> keyboardsByOwner= manager.findKeyboardsByOwner(owner);
+		assertEquals(2, keyboardsByOwner.size());
+		assertTrue(keyboardsByOwner.contains(k));
+		assertTrue(keyboardsByOwner.contains(k2));
+		
+	}
+	
 }
